44d92aff117f725c65d073bdb6aec765f1b55a6b
Seyma Cesen <cesenseyma@gmail.com>
2024-12-16 14:26:35 +0300

    Add project files.

---
+++ WebApi/Repositories/Config/BookConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Models;
+
+namespace WebApi.Repositories.Config
+{
+    public class BookConfig:IEntityTypeConfiguration<Book>
+    {
+       public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book { Id = 1, Title = "Karagöz ve Hacivat", Price = 75 },
+                new Book { Id = 2, Title = "Soytarı Çiçekleri", Price = 100 },
+                new Book { Id = 3, Title = "Beni Asla Bırakma", Price = 150 }
+                );
+        }
+    
+    }
+}
